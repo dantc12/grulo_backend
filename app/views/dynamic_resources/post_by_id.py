192b43974dacae310a934c75615d257c42d9bd0b
Dan Tsir Cohen <dantc12@gmail.com>
2021-01-03 01:04:16 +0200

    added comments

--- app/views/dynamic_resources/post_by_id.py
+++ app/views/dynamic_resources/post_by_id.py
@@ -2,6 +2,7 @@ from flask_restful import Resource, reqparse
 from mongoengine import DoesNotExist
 
 from app.models.posts_model import Posts
+from app.sessions_ids import sessions_ids
 
 
 class PostById(Resource):
@@ -22,13 +23,21 @@ class PostById(Resource):
     def post(self, post_id):  # add a comment
         parser = reqparse.RequestParser()
         parser.add_argument('session_id', required=True)
-        _ = parser.parse_args()
+        parser.add_argument('text', required=True)
+        args = parser.parse_args()
 
         try:
             p = Posts.objects.get(post_id=post_id)
         except DoesNotExist:
             return {"message": "Post doesn't exist."}, 500
         else:
-            response = {"message": "Found post successfully."}
-            response.update(p.json())
+            comment_index = len(p.comments)
+            p.comments += [{
+                "comment_index": comment_index,
+                "user_name": sessions_ids[args['session_id']],
+                "text": args['text'],
+                "liked_users_names": []
+            }]
+            response = {"message": "Added comment to post successfully."}
+            response.update(p.comments[-1])
             return response, 200
