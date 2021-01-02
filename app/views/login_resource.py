2efc5c373515bb8e15e3f015ec69e343aa87d549
Dan Tsir Cohen <dantc12@gmail.com>
2021-01-02 14:36:33 +0200

    sample login and signup pages

--- app/views/login_resource.py
+++ app/views/login_resource.py
@@ -1,6 +1,10 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 
 class Login(Resource):
     def get(self):
-        return "This is the login API"
+        parser = reqparse.RequestParser()
+        parser.add_argument('username', required=True)
+        parser.add_argument('password', required=True)
+        args = parser.parse_args()  # parse arguments to dictionary
+        return args, 200
