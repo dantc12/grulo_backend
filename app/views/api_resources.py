6b3f93931491a4e3b58da6a57b39253d65c4005c
Noam Goldface <noam.goldface@wsc-sports.com>
2021-01-02 18:30:22 +0200

    groups by coor

--- app/views/api_resources.py
+++ app/views/api_resources.py
@@ -1,6 +1,7 @@
 from app.views.is_alive import IsAlive
 from app.views.login_resource import Login
 from app.views.signup_resource import SignUp
+from app.views.groups_resource import GetByCoor
 
 api_resources = [
     {
@@ -14,5 +15,9 @@ api_resources = [
     {
         "name": SignUp,
         "path": '/signup'
+    },
+    {
+        "name": GetByCoor,
+        "path": '/groups/getByCoor'
     }
 ]
