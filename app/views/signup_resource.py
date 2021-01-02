91dc66a166d02720c176665f53f615cfc2dc2f67
Dan Tsir Cohen <dantc12@gmail.com>
2021-01-02 15:33:21 +0200

    signup progress

--- app/views/signup_resource.py
+++ app/views/signup_resource.py
@@ -1,4 +1,6 @@
 from flask_restful import Resource, reqparse
+from app.models import User
+import json
 
 
 class SignUp(Resource):
@@ -6,5 +8,25 @@ class SignUp(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument('username', required=True)
         parser.add_argument('password', required=True)
+        parser.add_argument('email', required=True)
+        parser.add_argument('address', required=True)
+        parser.add_argument('birthday')
+        parser.add_argument('phone')
+        parser.add_argument('gender')
+        parser.add_argument('bio')
         args = parser.parse_args()  # parse arguments to dictionary
-        return args, 200
+
+        u = User(
+            user_name=args.get('username'),
+            password=args.get('password'),
+            email=args.get('email'),
+            address=args.get('address')
+            # birthday=args.get('birthday') if args.get('birthday') else "",
+            # phone=args.get('phone') if args.get('phone') else "",
+            # gender=args.get('gender') if args.get('gender') else "",
+            # bio=args.get('bio') if args.get('bio') else ""
+        )
+        u.save()
+        print(str(User.objects.get(user_name='dan')))
+
+        return u.json(), 200
