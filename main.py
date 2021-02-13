921ddb2554ab4dbb68c5f76ed15b84b059c151ca
Dan Tsir Cohen <dantc12@gmail.com>
2021-02-13 22:07:23 +0200

    added and using config file

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from flask_restful import Api
 from mongoengine import connect
 
 from app.api_resources import api_resources
+from config import *
 
 grulo_app = Flask(__name__)
 grulo_api = Api(grulo_app)
 
-connect(host="20.196.3.185:27018", db="grulo")
+connect(host=MONGO_HOST, port=MONGO_PORT, db=DB_NAME)
 
 for resource in api_resources:
     grulo_api.add_resource(resource.get('name'), resource.get('path'))
 
 
 if __name__ == "__main__":
-    grulo_app.run(host='0.0.0.0', port=80, debug=True)
+    grulo_app.run(host=FLASK_HOST, port=FLASK_PORT, debug=True)
