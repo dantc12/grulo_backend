7beabae57b988bbe9883b92e47240cbe6346f3d7
Dan Tsir Cohen <dantc12@gmail.com>
2021-02-14 19:17:52 +0200

    new login resource

--- app/views/authentication/login_resource.py
+++ app/views/authentication/login_resource.py
@@ -1,4 +1,5 @@
-from flask_restful import Resource, reqparse
+from typing import Dict
+
 from mongoengine import DoesNotExist
 
 from app.models.users_model import Users
@@ -8,26 +9,22 @@ import binascii
 from app.sessions_ids import sessions_ids
 
 
-class Login(Resource):
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('username', required=True)
-        parser.add_argument('password', required=True)
-        args = parser.parse_args()  # parse arguments to dictionary
-
-        try:
-            u = Users.objects.get(username=args.get('username'))
-        except DoesNotExist:
-            return {"message": "User or password are incorrect."}, 500
+def login(login_info: Dict):
+    try:
+        u = Users.objects.get(user_name=login_info.get('user_name'))
+    except DoesNotExist:
+        return {"message": "User or password are incorrect."}, 500
+    else:
+        if u["password"] == login_info.get('password'):
+            for session_id, user_name in sessions_ids.items():
+                if u.user_name == user_name:
+                    return {
+                        "session_id": session_id
+                    }, 200
+            session_id = str(binascii.hexlify(os.urandom(24)).decode('ascii'))
+            sessions_ids[session_id] = login_info.get('user_name')
+            return {
+                "session_id": session_id
+            }, 200
         else:
-            if u["password"] == args.get('password'):
-                for session_id, username in sessions_ids.items():
-                    if u.username == username:
-                        return {"message": "Login successful.",
-                                "session_id": session_id}, 200
-                session_id = str(binascii.hexlify(os.urandom(24)).decode('ascii'))
-                sessions_ids[session_id] = args.get('username')
-                return {"message": "Login successful.",
-                        "session_id": session_id}, 200
-            else:
-                return {"message": "User or password are incorrect."}, 500
+            return {"message": "User or password are incorrect."}, 500
