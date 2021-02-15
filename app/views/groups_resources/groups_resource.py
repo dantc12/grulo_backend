787496fb797eab0d56d5f6dbd773b987a7340557
Dan Tsir Cohen <dantc12@gmail.com>
2021-02-15 06:08:43 +0200

    added get all groups resource

--- app/views/groups_resources/groups_resource.py
+++ app/views/groups_resources/groups_resource.py
@@ -1,12 +1,12 @@
-from flask_restful import Resource, reqparse
 from app.models.groups_model import Groups
 import json
 from app.sessions_ids import sessions_ids
 
-class GetAllGroups(Resource):
-    def get(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('session_id', required=True)
-        _ = parser.parse_args()  # parse arguments to dictionary
-        groups = Groups.objects()
-        return json.loads(groups.to_json()), 200
+
+def get_all_groups(session_id: str):
+    if session_id not in sessions_ids.keys():
+        return {
+            "message": "Not logged in"
+        }, 400
+    groups = Groups.objects()
+    return json.loads(groups.to_json()), 200
