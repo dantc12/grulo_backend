584d9964b8b5bdcdaba6a98c8bd31111daf0e790
Dan Tsir Cohen <dantc12@gmail.com>
2021-02-16 19:29:30 +0200

    created last resource - posts by coor

--- app/views/posts_resources/posts_by_coor.py
+++ app/views/posts_resources/posts_by_coor.py
@@ -1,31 +1,25 @@
-from flask_restful import Resource, reqparse
 from mongoengine import DoesNotExist
 
 from app.models.groups_model import Groups
 from app.models.posts_model import Posts
-from app.utils import get_google_groups_by_coor
+from app.utils import get_google_groups_by_coor, check_if_logged_in
 
 
-class GetPostsByCoor(Resource):
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('coordinates', required=True)
-        parser.add_argument('session_id', required=True)
-        args = parser.parse_args()  # parse arguments to dictionary
+def get_posts_by_coor(session_id: str, coordinates: str):
+    message, return_code = check_if_logged_in(session_id)
+    if return_code == 400:
+        return message, return_code
 
-        locs = get_google_groups_by_coor(args.get('coordinates'))
+    locs = get_google_groups_by_coor(coordinates)
 
-        res_posts = []
-        for loc in locs:
-            try:
-                group = Groups.objects.get(groupid=loc.get("place_id"))
-            except DoesNotExist:
-                raise Exception("Issue with getting google group from grulo groups db.")
+    res_posts = []
+    for loc in locs:
+        try:
+            group = Groups.objects.get(group_id=loc.get("place_id"))
+        except DoesNotExist:
+            return {"message": "Issue with getting google group from grulo groups db."}, 401
 
-            group_posts = [Posts.objects.get(post_id=post_id) for post_id in group.post_ids]
-            res_posts += group_posts
+        group_posts = [Posts.objects.get(post_id=post_id) for post_id in group.post_ids]
+        res_posts += group_posts
 
-        return {
-            "message": "retrieved posts successfuly.",
-            "posts": res_posts
-        }, 200
+    return res_posts, 200
