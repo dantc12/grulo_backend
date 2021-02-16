99d287b29960436689ba65719aaa736e56a2b375
Dan Tsir Cohen <dantc12@gmail.com>
2021-02-16 19:08:53 +0200

    added new google func

--- app/utils.py
+++ app/utils.py
@@ -4,7 +4,7 @@ from typing import Dict, List
 import requests
 
 
-def get_google_maps_coors(coor_str: str) -> List[Dict]:
+def get_google_groups_by_coor(coor_str: str) -> List[Dict]:
     url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" \
           "{}&key=AIzaSyBu-btSrvUAGaR-GHHfdk0kjlwiO91XV8k&language=iw".format(coor_str)
     response = requests.request("GET", url, headers={}, data={})
@@ -12,7 +12,7 @@ def get_google_maps_coors(coor_str: str) -> List[Dict]:
     return locations_json.get("results")
 
 
-def get_google_maps_group(group_id: str):
+def get_google_group_by_id(group_id: str):
     url = "https://maps.googleapis.com/maps/api/geocode/json?place_id={}" \
           "&key=AIzaSyBu-btSrvUAGaR-GHHfdk0kjlwiO91XV8k&language=iw".format(group_id)
     response = requests.request("GET", url, headers={}, data={})
@@ -20,3 +20,12 @@ def get_google_maps_group(group_id: str):
     group_name = response_json["results"][0]["formatted_address"]
     group_type = response_json["results"][0]["types"][0]
     return group_name, group_type
+
+
+def get_google_groups_by_addr(address: str):
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address={}" \
+          "&key=AIzaSyBu-btSrvUAGaR-GHHfdk0kjlwiO91XV8k&language=iw".format(address)
+    response = requests.request("GET", url, headers={}, data={})
+
+    search_results = json.loads(response.text)["results"]
+    return search_results
