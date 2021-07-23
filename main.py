e0e16498c7cfb3b9809b83d63af06eb159866132
Dan Tsir Cohen <dantc12@gmail.com>
2021-07-23 08:08:32 +0300

    CORS fix

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from mongoengine import connect
 
 from config import *
+from flask_cors import CORS
 import connexion
 
 grulo_app = connexion.App(__name__)
+CORS(grulo_app.app)
 grulo_app.add_api('swagger.yml')
 
 connect(host=MONGO_HOST, port=MONGO_PORT, db=DB_NAME)
