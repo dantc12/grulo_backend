d3b7031f62bcfe60a39c7dce524fa70ea8f75b04
Dan Tsir Cohen <dantc12@gmail.com>
2021-08-06 14:53:05 +0300

    posts crud and routers

---
+++ app/database_crud/posts.py
@@ -0,0 +1,77 @@
+from typing import List, Optional
+
+from .. import exceptions, schemas, models
+from .users import get_user_by_name
+from .groups import get_group_by_id, get_group_by_name
+
+
+def get_post_by_id(post_id: str) -> models.Post:
+    if models.Post.objects(post_id=post_id):
+        return models.Post.objects(post_id=post_id)[0]
+    raise exceptions.PostNotFound(post_id)
+
+
+# TODO add edit post
+
+
+def add_comment_to_post(post_id: str, comment: schemas.CommentCreate) -> models.Post:
+    post = get_post_by_id(post_id)
+    created_comment = schemas.Comment(index=len(post.comments),
+                                      likes=[],
+                                      **comment.dict())
+    post.comments.append(created_comment.dict())
+    post.save()
+    return post
+
+
+def create_post(post: schemas.PostCreate) -> models.Post:
+    taken_post_ids = [post.post_id for post in models.Post.objects]
+    post_id = 1
+    while str(post_id) in taken_post_ids:
+        post_id += 1
+    post_id = str(post_id)
+    post = models.Post(post_id=post_id, **post.dict())
+    post.save()
+
+    # add to user's posts
+    posting_user = models.User.objects(username=post.username)[0]
+    posting_user.update(post_ids=posting_user.post_ids + [post_id])
+    # add to group's posts
+    posted_group = models.Group.objects(group_name=post.group_name)[0]
+    posted_group.update(post_ids=posted_group.post_ids + [post_id])
+
+    return post
+
+
+def get_posts_for_user(username: str, limit: Optional[int] = None) -> List[models.Post]:
+    user = get_user_by_name(username)
+    posts = []
+    for group_id in user.group_ids:
+        group = get_group_by_id(group_id)
+        group_posts = [get_post_by_id(post_id) for post_id in group.post_ids]
+        if limit is not None and len(posts) + len(group_posts) > limit:
+            i = 0
+            while len(posts) < limit:
+                posts.append(group_posts[i])
+                i += 1
+            return posts
+        else:
+            posts.extend(group_posts)
+    return posts
+
+
+def get_posts_by_user(username: str) -> List[models.Post]:
+    user = get_user_by_name(username)
+    return [get_post_by_id(post_id) for post_id in user.post_ids]
+
+
+def get_posts_by_group_name(group_name: str) -> List[models.Post]:
+    group = get_group_by_name(group_name)
+    return [get_post_by_id(post_id) for post_id in group.post_ids]
+
+
+def get_posts_by_group_id(group_id: str) -> List[models.Post]:
+    group = get_group_by_id(group_id)
+    return [get_post_by_id(post_id) for post_id in group.post_ids]
+
+# TODO add get posts by coordinates
---
+++ app/routers/posts.py
@@ -0,0 +1,89 @@
+from typing import Optional, List
+
+from fastapi import APIRouter, Depends, HTTPException
+
+from .. import database, exceptions
+from .. import schemas
+from ..database_crud import posts
+
+router = APIRouter(
+    prefix="/posts",
+    tags=["posts"],
+    dependencies=[Depends(database.connect_to_db)]  # TODO add the security part
+)
+
+
+@router.post("/")
+def post_new_post(post: schemas.PostCreate) -> schemas.Post:
+    try:
+        new_post = posts.create_post(post)
+        return schemas.Post(**new_post.to_dict())
+    except Exception as e:
+        raise HTTPException(500, str(e))
+
+
+@router.get("/{post_id}")
+def get_post_by_id(post_id: Optional[str] = None) -> schemas.Post:
+    try:
+        post = posts.get_post_by_id(post_id)
+        return schemas.Post(**post.to_dict())
+    except exceptions.PostNotFound as e:
+        raise HTTPException(404, str(e))
+    except Exception as e:
+        raise HTTPException(500, str(e))
+
+
+@router.get("/")
+def get_posts(group_name: Optional[str] = None,
+              group_id: Optional[str] = None,
+              username: Optional[str] = None) -> List[schemas.Post]:
+    if group_name is None and group_id is None and username is None:
+        raise HTTPException(400, "Must use an identifier for query.")
+    try:
+        if group_name is not None:
+            found_posts = posts.get_posts_by_group_name(group_name)
+        elif group_id is not None:
+            found_posts = posts.get_posts_by_group_id(group_id)
+        else:  # username is not None:
+            found_posts = posts.get_posts_by_user(username)
+        return [schemas.Post(**post.to_dict()) for post in found_posts]
+    except exceptions.GroupNotFound as e:
+        raise HTTPException(404, str(e))
+    except exceptions.UserNotFound as e:
+        raise HTTPException(404, str(e))
+    except Exception as e:
+        raise HTTPException(500, str(e))
+
+
+@router.put("/comment/{post_id}")
+def add_comment_to_post(post_id: str, comment: schemas.CommentCreate) -> schemas.Post:
+    try:
+        post = posts.add_comment_to_post(post_id, comment)
+        return schemas.Post(**post.to_dict())
+    except exceptions.PostNotFound as e:
+        raise HTTPException(404, str(e))
+    except Exception as e:
+        raise HTTPException(500, str(e))
+
+
+@router.get("/for_user/{username}")
+def get_all_posts_for_user(username: str, limit: Optional[int] = None) -> List[schemas.Post]:
+    try:
+        posts_for_user = posts.get_posts_for_user(username, limit)
+        return [schemas.Post(**post.to_dict()) for post in posts_for_user]
+    except exceptions.UserNotFound as e:
+        raise HTTPException(404, str(e))
+    except Exception as e:
+        raise HTTPException(500, str(e))
+
+# @router.get("/by_coor")
+# def get_posts_by_coordinates(group_name: str) -> List[schemas.Post]:
+#     try:
+#         group_posts = posts.get_posts_by_group_name(group_name)
+#         return [schemas.Post(**post.to_dict()) for post in group_posts]
+#     except exceptions.GroupNotFound as e:
+#         raise HTTPException(404, str(e))
+#     except Exception as e:
+#         raise HTTPException(500, str(e))
+
+# TODO add get posts by coordinates
