3ed9e3c527f6c17c66417dd74e53c2f20ce2d18c
Dan Tsir Cohen <dantc12@gmail.com>
2021-08-06 14:52:42 +0300

    schemas and models

---
+++ app/schemas.py
@@ -0,0 +1,93 @@
+import datetime
+from typing import Optional, List
+from pydantic import BaseModel
+
+
+class UserBase(BaseModel):
+    username: str
+    email: str
+
+    address: Optional[str]
+    first_name: Optional[str]
+    last_name: Optional[str]
+    birthday: Optional[datetime.datetime]
+    phone: Optional[str]
+    gender: Optional[str]
+    bio: Optional[str]
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    group_ids: List[str]
+    post_ids: List[str]
+
+    class Config:
+        orm_mode = True
+
+
+class LikeBase(BaseModel):
+    username: str
+
+
+class LikeCreate(LikeBase):
+    pass
+
+
+class Like(LikeBase):
+    pass
+
+
+class CommentBase(BaseModel):
+    username: str
+    text: str
+
+
+class CommentCreate(CommentBase):
+    pass
+
+
+class Comment(CommentBase):
+    index: int
+    likes: List[Like]
+
+
+class PostBase(BaseModel):
+    username: str
+    group_name: str
+    text: str
+
+
+class PostCreate(PostBase):
+    pass
+
+
+class Post(PostBase):
+    post_id: str
+    post_date: datetime.datetime
+    last_update: datetime.datetime
+    comments: List[Comment]
+    likes: List[Like]
+
+    class Config:
+        orm_mode = True
+
+
+class GroupBase(BaseModel):
+    group_name: str
+    group_type: str
+
+
+class GroupCreate(GroupBase):
+    pass
+
+
+class Group(GroupBase):
+    group_id: str
+    users: List[str]
+    posts_ids: List[str]
+
+    class Config:
+        orm_mode = True
