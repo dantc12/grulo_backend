a2e27f1a87c0188db847d869b5ce177218a8ebfc
Dan Tsir Cohen <dantc12@gmail.com>
2021-08-12 21:23:40 +0300

    CORS support

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import users, posts, groups
 
 app = FastAPI()
+
+app.add_middleware(CORSMiddleware,
+                   allow_origins=["*"],
+                   allow_credentials=False,
+                   allow_methods=["*"],
+                   allow_headers=["*"])
 
 app.include_router(users.router)
 app.include_router(posts.router)
