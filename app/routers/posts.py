fb16f6e46c3c245e3d01b1158c4acf4d41f7fd3a
Dan Tsir Cohen <dantc12@gmail.com>
2021-08-30 10:10:42 +0300

    range search should be working

--- app/routers/posts.py
+++ app/routers/posts.py
@@ -4,8 +4,9 @@ from fastapi import APIRouter, HTTPException, Depends
 
 from .. import exceptions
 from .. import schemas
-from ..database_crud import posts, models
+from ..database_crud import posts, models, groups
 from ..dependencies import verify_logged_in, get_current_user
+from ..globals import reverse_geocoder
 
 router = APIRouter(
     prefix="/posts",
@@ -24,9 +25,6 @@ def post_new_post(post: schemas.PostCreate, user: models.User = Depends(get_curr
         raise HTTPException(404, str(e))
     except Exception as e:
         raise HTTPException(500, str(e))
-
-
-# TODO Continue here adding current user dependency when needed
 
 
 @router.get("/{post_id}", response_model=schemas.Post)
@@ -70,15 +68,37 @@ def add_comment_to_post(post_id: str, comment: schemas.CommentCreate,
 
 
 @router.post("/feed/", response_model=List[schemas.Post])
-def get_user_feed(range: schemas.RangeSearch, user: models.User = Depends(get_current_user)) -> \
+def get_user_feed(start: int = 0, end: Optional[int] = None, user: models.User = Depends(get_current_user)) -> \
         List[schemas.Post]:
     try:
         posts_for_user = posts.get_user_feed(user)
         sorted_posts = sorted(posts_for_user, key=lambda x: x.last_update)[::-1]
-        if range.max is not None:
-            return sorted_posts[range.min:range.max]
-        else:
-            return sorted_posts[range.min:]
+        if end is None:
+            return sorted_posts[start:]
+        return sorted_posts[start:end]
+    except exceptions.NotFoundException as e:
+        raise HTTPException(404, str(e))
+    except Exception as e:
+        raise HTTPException(500, str(e))
+
+
+@router.post("/explore/", response_model=List[schemas.Post])
+def explore_posts_by_coor(start: int = 0, end: Optional[int] = None, lat: str = "32.08217107033524",
+                          lon: str = "34.80586379620104") -> \
+        List[schemas.Post]:
+    try:
+        places = reverse_geocoder.reverse_geocode(lat, lon)
+        existing_groups = []
+        for place in places:
+            try:
+                existing_groups.append(groups.get_group_by_name(place.name))
+            except exceptions.NotFoundException:
+                continue
+        posts_for_user = [post for group in existing_groups for post in posts.get_posts_by_group_name(group.group_name)]
+        sorted_posts = sorted(posts_for_user, key=lambda x: x.last_update)[::-1]
+        if end is None:
+            return sorted_posts[start:]
+        return sorted_posts[start:end]
     except exceptions.NotFoundException as e:
         raise HTTPException(404, str(e))
     except Exception as e:
